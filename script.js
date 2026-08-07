403d66d908f723d7130029805600870edb55bfa5
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-07 09:57:49 +0530

    studentList

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-let arr = [12, 45, 7, 89, 23];
+const input = document.getElementById("studentName");
+const addBtn = document.getElementById("addBtn");
+const studentList = document.getElementById("studentList");
+const count = document.getElementById("count");
 
-let largest = arr[0];
+let totalStudents = 0;
 
-for (let i = 1; i <= arr.length; i++) {
-    if(arr[i] > largest) {
-        largest = arr[i]
+addBtn.addEventListener("click", function () {
+
+    let name = input.value.trim();
+
+    if (name === "") {
+        alert("Please enter a student name");
+        return;
     }
-}
 
-console.log(largest)
+    let li = document.createElement("li");
+    li.innerText = name + " ";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+        totalStudents--;
+        count.innerText = totalStudents;
+    });
+
+    li.appendChild(deleteBtn);
+    studentList.appendChild(li);
+
+    totalStudents++;
+    count.innerText = totalStudents;
+
+    input.value = "";
+});
