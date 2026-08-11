753394c58067ecfc5ea1cdd6b62c330a3779ae23
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-11 21:36:29 +0530

    a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let a = document.querySelector("a");
-a.removeAttribute("href")
-//a.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeBxRVDN84QussXLTTKXMYcU4w3vo4tOqbGpkJZp4Bb6B7prS_yyvQDeY&s=10")
-console.dir(a);
+let li = document.querySelectorAll("li")
+li.forEach(function(val) {
+    console.log(val.textContent);
+});
