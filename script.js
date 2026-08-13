7e241f6a586a8eb83e9f7e3bd1404d7658b14192
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-13 13:57:21 +0530

    new

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let li = document.querySelectorAll("ul li:nth-child(2n)");
-li.forEach(function(val) {
-    val.classList.add("lol");
-})
+let input = document.querySelector("input");
+
+input.addEventListener("input", function(val) {
+    if(val.data !== " " && val.data !== null) {
+        console.log(val.data);
+    } 
+});
