fcbdc4c2754dcea19d8ed3c1faa52ac2d48acf71
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-14 22:46:57 +0530

    new

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let main = document.querySelector("#main")
+let main = document.querySelector("#main");
 let form = document.querySelector("form");
-let input = document.querySelectorAll("input")
+let input = document.querySelectorAll("input");
 
 form.addEventListener("submit", function(det) {
     det.preventDefault();
-
+    
     let card = document.createElement("div");
     card.classList.add("card");
 
@@ -14,21 +14,25 @@ form.addEventListener("submit", function(det) {
     let img = document.createElement("img");
     img.setAttribute("src", input[0].value);
     profile.appendChild(img);
-    card.appendChild(profile)
+    card.appendChild(profile);
 
     let h2 = document.createElement("h2");
     h2.textContent = input[1].value;
     card.appendChild(h2);
 
-    let h4 = document.createElement("h4");
-    h4.textContent = input[2].value;
-    card.appendChild(h4)
+    let h3 = document.createElement("h3");
+    h3.textContent = input[2].value;
+    card.appendChild(h3);
 
-    let h5 = document.createElement("h5");
-    h5.textContent = input[3].value;
-    card.appendChild(h5);
+    let p = document.createElement("p")
+    p.textContent = input[3].value;
+    card.appendChild(p);
 
     main.appendChild(card);
 
-    console.log(card)
+    input.forEach(function(val) {
+        if (val.type !== "submit") {
+            val.value = "";
+        }
+    });
 });
