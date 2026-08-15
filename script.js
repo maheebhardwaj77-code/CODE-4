c1af11dd695aea79f53b0d3ac0fd6055acef6d3f
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-15 22:54:48 +0530

    hello

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let a = document.querySelector("#a");
+let inp = document.querySelector("input")
+let h2 = document.querySelector("h2 span")
 
-window.addEventListener("mousemove", function(det) {
-    a.style.top = det.clientY + "px";
-    a.style.left = det.clientX + "px";
+inp.addEventListener("input", function(det) {
+    let left = 20 - inp.value.length;
+    if(left < 0) {
+        h2.textContent = left
+        h2.style.color = "red"
+    }
+    else {
+        h2.textContent = left;
+        h2.style.color = "white"
+    }
 });
