76b31b6a2a03502db29b819d4b71907769a87841
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-15 11:50:46 +0530

    j

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let a = document.querySelector("#a");
-let h2 = document.querySelector("h2");
 
-a.addEventListener("mouseover", function() {
-    h2.style.backgroundColor = "rgb(17, 94, 90)";
-    h2.style.fontSize = "30px"
-});
-
-a.addEventListener("mouseout", function() {
-    h2.style.backgroundColor = "rgb(32, 169, 162)";
-    h2.style.fontSize = "25px"
+window.addEventListener("mousemove", function(det) {
+    a.style.top = det.clientY + "px";
+    a.style.left = det.clientX + "px";
 });
