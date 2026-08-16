6eadf02717a1a77727b64b0f1ea0f3ec861eb1e5
Mahee Bhardwaj <maheebhardwaj77@gmail.com>
2026-08-16 17:46:23 +0530

    same

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let mail = document.querySelector("#email");
-let pass = document.querySelector("#password");
-let form = document.querySelector("#loginForm");
-let btn = document.querySelector("button");
+let form = document.querySelector("#loginForm")
+let email = document.querySelector("#email")
+let pass = document.querySelector("#password")
 
 form.addEventListener("submit", function(det) {
     det.preventDefault();
@@ -9,33 +8,33 @@ form.addEventListener("submit", function(det) {
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    let mailans = emailRegex.test(mail.value);
-    let passans = passwordRegex.test(pass.value)
+    let emailans = emailRegex.test(email.value);
+    let passans = passwordRegex.test(pass.value);
 
     let isValid = true;
 
-    if(!mailans) {
-        document.querySelector("#emailError").textContent = "invalid Email !"
-        mail.value = "";
+    if(!emailans) {
+        document.querySelector("#emailError").textContent = "Invalid Email !";
+        email.value = "";
         isValid = false;
     }
     else {
-        document.querySelector("#emailError").textContent = ""
-    }
+        document.querySelector("#emailError").textContent = "";
+    };
 
     if(!passans) {
-        document.querySelector("#passwordError").textContent = "invalid password !"
+        document.querySelector("#passwordError").textContent = "Invalid Password !";
         pass.value = "";
         isValid = false;
     }
     else {
-        document.querySelector("#passwordError").textContent = ""
-    }  
-    
+        document.querySelector("#passwordError").textContent = "";
+    };
+
     if(isValid) {
-        document.querySelector("#successMessage").textContent = "Everything is Correct !"
+        document.querySelector("#successMessage").textContent = "Everything is Correct";
     }
     else {
-        document.querySelector("#successMessage").textContent = ""
-    }
-});
+        document.querySelector("#successMessage").textContent = "";
+    };
+})
